8aaa9a145ced75747f6bfab499eb05cee2acae71
lonelynight-dark <52921466+lonelynight-dark@users.noreply.github.com>
2021-12-15 18:14:58 +0700

    add SlangProcess.java

---
+++ src/Process/SlangProcess.java
@@ -0,0 +1,10 @@
+package Process;
+
+/**
+ * Process
+ * Created by Hieu Tran Trung
+ * Date 12/15/2021 - 6:14 PM
+ * Description: ...
+ */
+public class SlangProcess {
+}
--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -9,5 +9,8 @@ package com.main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+
+
     }
 }
