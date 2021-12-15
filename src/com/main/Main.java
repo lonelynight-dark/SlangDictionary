bfc4467a50d86424fccd803a6c083b418e4339c6
lonelynight-dark <52921466+lonelynight-dark@users.noreply.github.com>
2021-12-15 18:12:35 +0700

    first initialize

---
+++ src/com/main/Main.java
@@ -0,0 +1,13 @@
+package com.main;
+
+/**
+ * com.main
+ * Created by Hieu Tran Trung
+ * Date 12/15/2021 - 6:06 PM
+ * Description: ...
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
